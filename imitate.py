d131037c7ab3e802e55935060c8618f9e958c2d4
Tyler <reicht1>
2018-03-15 20:02:08 -0400

    Bot can no longer @ people

--- imitate.py
+++ imitate.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import errors
 import json
 import markovify
 import os
@@ -38,6 +39,7 @@ class imitate:
         username = " ".join(username)
         currentServer = ctx.message.server
         user = currentServer.get_member_named(username)
+        mentionRegex = r"<@![0-9]*>"
         
         #continue only if user actually exists
         if user is None:
@@ -55,17 +57,30 @@ class imitate:
             try:
                 jsonData = json.load(file)
                 model = markovify.NewlineText.from_json(str(jsonData))
-              
                 charsLeft = sentenceSize
                 while charsLeft > 20:
-                    #await self.bot.say(str(charsLeft) + " left")
                     resultMessage = model.make_short_sentence(charsLeft)
+                    
+                    #clean out any mentions that may have found their way in           
+                    while re.search(mentionRegex, resultMessage) is not None:
+                        foundMention = re.search(mentionRegex, resultMessage).group(0)
+                        #print("foundMention is" + foundMention)
+                        mentionString = re.search(r"[0-9]+", foundMention).group(0)
+                        #print("mentionString is" + mentionString)
+                        try:
+                            mentionName = await self.bot.get_user_info(mentionString)
+                            mentionName = mentionName.display_name
+                            resultMessage = resultMessage.replace(foundMention, mentionName)
+                        except discord.errors.NotFound:
+                            resultMessage = resultMessage.replace(foundMention, "[unknown user]")
+                        except discord.errors.HTTPException:
+                            resultMessage = resultMessage.replace(foundMention, "[unknown user]")
+                    
                     if len(resultMessage) > 0:
                         await self.bot.say(resultMessage)
                         charsLeft -= len(resultMessage)
                     else:
                         continue
-                #await self.bot.say("Done!")
             except ValueError:
                 print("ERROR: imitate: Could not get markov model from user JSON file!")
                 await self.bot.say("ERROR: imitate: Could not get markov model from user JSON file!")
@@ -75,20 +90,31 @@ class imitate:
                 await self.bot.say("User does not have any information about them yet.")
      
     async def on_message_create(self, message):
-        print(message.author.name + ": " + message.content + '\n')
         
-        content = message.content
+        content = message.clean_content
+        
         userID = message.author.id
-        exclusionString = "^.((re|un|)load )?imitate"
+        exclusionString = r"^[!?\\\/]" #exclude lines with commands
         
         #if we need to ignore the message...ignore it
         exclude = re.search(exclusionString, content)
         
-        if exclude:
+        #if content should be ignored, or is basically nothing, ignore it
+        if exclude or (content.replace(" ", "") is ""):
             return;
         
+        print(message.author.name + ": " + message.content + '\n')
+        
+        
         #get new model
-        model = markovify.NewlineText(content, retain_original=False)
+        if content is not "":
+            try:
+                model = markovify.NewlineText(content, retain_original=False)
+            except KeyError:
+                print("ERROR: Imitate: could not add message to model. It may have been blank.")
+                return
+        else:
+            return
         
         #make sure that the user has a profile, if they do, get info from it.
         if await self.profileExists(userID):
